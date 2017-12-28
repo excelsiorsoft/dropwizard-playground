e0e3858c54c3aa49cb993812361864d054585ca6
sleyzerzon <senya22@yahoo.com>
2017-12-28 16:34:33 -0500

    added json support

--- simple-service/src/main/java/com/excelsiorsoft/examples/SimpleServiceApplication.java
+++ simple-service/src/main/java/com/excelsiorsoft/examples/SimpleServiceApplication.java
@@ -4,6 +4,9 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class SimpleServiceApplication extends Application<SimpleServiceConfiguration> {
 
     public static void main(final String[] args) throws Exception {
@@ -23,7 +26,10 @@ public class SimpleServiceApplication extends Application<SimpleServiceConfigura
     @Override
     public void run(final SimpleServiceConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+
+        DateFormat eventDateFormat = new SimpleDateFormat(configuration.getDateFormat());
+        environment.getObjectMapper().setDateFormat(eventDateFormat);
+
     }
 
 }
