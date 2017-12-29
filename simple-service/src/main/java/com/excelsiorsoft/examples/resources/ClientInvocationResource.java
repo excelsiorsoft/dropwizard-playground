a4ad6458c3009b6c341608b2d7dd486ad6864993
sleyzerzon <senya22@yahoo.com>
2017-12-29 15:28:05 -0500

    testing redirection use case

--- simple-service/src/main/java/com/excelsiorsoft/examples/resources/ClientInvocationResource.java
+++ simple-service/src/main/java/com/excelsiorsoft/examples/resources/ClientInvocationResource.java
@@ -1,9 +1,38 @@
 package com.excelsiorsoft.examples.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("client")
+@Produces(MediaType.APPLICATION_JSON)
 public class ClientInvocationResource {
+
+    private final static Logger logger = LoggerFactory.getLogger(ClientInvocationResource.class);
+
+    @GET
+    @Path("redirect")
+    public void getRedirect() {
+
+        HelloWorldResource.CreateRoleRequest item = new HelloWorldResource.CreateRoleRequest();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080/api/helloworld/redirect");
+
+        Response response = target
+                .request()
+                .put(Entity.json(item));
+
+        logger.debug("Here's my response: {}", response);
+    }
 
 
 }
--- simple-service/src/main/java/com/excelsiorsoft/examples/resources/HelloWorldResource.java
+++ simple-service/src/main/java/com/excelsiorsoft/examples/resources/HelloWorldResource.java
@@ -1,12 +1,15 @@
 package com.excelsiorsoft.examples.resources;
 
 import com.excelsiorsoft.examples.compress.Compress;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 
 @Path("helloworld")
@@ -29,5 +32,23 @@ public class HelloWorldResource {
 
         String str = "very long string to be compressed during request processing";
         return str;
+    }
+
+    @PUT
+    @Path("redirect")
+    public Response getRedirect() {
+        return Response.notModified().build();
+    }
+
+    @Data
+    public static class CreateRoleRequest{
+        private String fieldA;
+        private String fieldB;
+    }
+
+    @Data
+    public static class CreateRoleResponse{
+        private String fieldA;
+        private String fieldB;
     }
 }
